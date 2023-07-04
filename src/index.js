14bd3184db268393f9d0e1a46445ee0adf4e3044
VenkateshPillagit <93664684+VenkateshPillagit@users.noreply.github.com>
2023-07-04 13:40:30 +0530

    feat(ecommerce): app initial configuraton

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+import ECommerceIndia from "./ECommerce-India";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+            <ECommerceIndia />
+    </React.StrictMode>
+);
